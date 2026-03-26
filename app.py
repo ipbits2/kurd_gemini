ac14564c07b84bf51fdca9d5cfa4c2dc83c3aab5
ipbits2 <peshwarf@yahoo.com>
2026-03-26 23:56:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,13 @@ system_instruction = """
 ٣. تۆ خەلکێ دەڤەرا بەهدینانی (سەرسنک، دهۆک، ئامێدی) و کلتۆرێ وان زۆر باش دناسی.
 ٤. ب ڕێز و حورمەت بەرسڤێ بدە.
 """
-
+# ئەڤە ئەو پشکەیە یا کو دبێژیتە گوگل چو تشتان بلۆک نەکە (Safety Settings)
+safety_settings = [
+    {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_NONE"},
+    {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_NONE"},
+    {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_NONE"},
+    {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_NONE"},
+]
 model = genai.GenerativeModel(
     model_name="gemini-1.5-flash",
     system_instruction=system_instruction, safety_settings=safety_settings )
